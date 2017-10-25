fa1945a337e024e4f270c52cedfd18fae662b0a7
tshagel <tshagel@ucalgary.ca>
2017-10-25 13:22:40 -0600

    Moved cart functionality to a new CS file.

--- CPSC481_Prototype/MainWindow.xaml.cs
+++ CPSC481_Prototype/MainWindow.xaml.cs
@@ -25,37 +25,5 @@ namespace CPSC481_Prototype
             InitializeComponent();
         }
 
-        private void Show_Cart_Border_MouseDown(object sender, MouseButtonEventArgs e)
-        {
-            if(Cart_Stack_Panel.Visibility == Visibility.Collapsed)
-            {
-                Cart_Stack_Panel.Visibility = Visibility.Visible;
-                Show_Cart_Label.Content = "Hide Cart";
-            } else
-            {
-                Cart_Stack_Panel.Visibility = Visibility.Collapsed;
-                Show_Cart_Label.Content = "Show Cart";
-            }
-        }
-
-        private void Requirement_Popup_MouseDown(object sender, MouseButtonEventArgs e)
-        {
-            if (Requirement_Popup.Visibility == Visibility.Visible)
-            {
-                Requirement_Popup.Visibility = Visibility.Hidden;
-            }
-        }
-
-        private void Show_Complete_CheckBox_Checked(object sender, RoutedEventArgs e)
-        {
-            Show_Completed_Stack_Panel.Visibility = Visibility.Visible;
-        }
-
-        private void Show_Complete_CheckBox_Unchecked(object sender, RoutedEventArgs e)
-        {
-            Show_Completed_Stack_Panel.Visibility = Visibility.Collapsed;
-        }
-
-
     }
 }
---
+++ CPSC481_Prototype/MainWindow_Cart_Functionality.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
+
+namespace CPSC481_Prototype
+{
+    public partial class MainWindow : Window
+    {
+        private void Show_Cart_Border_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (Cart_Stack_Panel.Visibility == Visibility.Collapsed)
+            {
+                Cart_Stack_Panel.Visibility = Visibility.Visible;
+                Show_Cart_Label.Content = "Hide Cart";
+            }
+            else
+            {
+                Cart_Stack_Panel.Visibility = Visibility.Collapsed;
+                Show_Cart_Label.Content = "Show Cart";
+            }
+        }
+
+        private void Requirement_Popup_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (Requirement_Popup.Visibility == Visibility.Visible)
+            {
+                Requirement_Popup.Visibility = Visibility.Hidden;
+            }
+        }
+
+        private void Show_Complete_CheckBox_Checked(object sender, RoutedEventArgs e)
+        {
+            Show_Completed_Stack_Panel.Visibility = Visibility.Visible;
+        }
+
+        private void Show_Complete_CheckBox_Unchecked(object sender, RoutedEventArgs e)
+        {
+            Show_Completed_Stack_Panel.Visibility = Visibility.Collapsed;
+        }
+    }
+}
