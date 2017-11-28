ba25e8cf63eff4319c9d9d538923da7772f12931
tshagel <tshagel@ucalgary.ca>
2017-11-28 13:50:39 -0700

    Initial Commit

--- CPSC481_Prototype/MainWindow.xaml.cs
+++ CPSC481_Prototype/MainWindow.xaml.cs
@@ -27,7 +27,12 @@ namespace CPSC481_Prototype
             DataContext = this;
             Course_Selector_Items.ItemsSource = CourseSelectorCourses.instance.visable;
             Cart_Items.ItemsSource = CartSelections.instance.visible;
-            Schedule_Items.ItemsSource = ScheduleSelections.instance.visible;    
+            Schedule_Items.ItemsSource = ScheduleSelections.instance.visible;
+
+            Messages_Box.ItemsSource = Messages.messages;
+            Messages.AddMessage("Message 1");
+            Messages.AddMessage("Message 2");
+            Messages.AddMessage("Message 3 is a really long message that should \nbe wrapped or it will end up being very wide");
         }
 
         private void Tutorial_Button_Click(object sender, RoutedEventArgs e)
