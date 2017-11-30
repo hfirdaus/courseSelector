b1911e9d168a8ef604605c43c1ec3e35b5c9be33
tshagel <tshagel@ucalgary.ca>
2017-11-30 13:53:10 -0700

    Need to remove the messages at a good time.

--- CPSC481_Prototype/Messages.cs
+++ CPSC481_Prototype/Messages.cs
@@ -66,7 +66,7 @@ namespace CPSC481_Prototype
         }
     }
 
-    class Message
+    public class Message
     {
         public string text { get; set; }
         public ICommand clearMsg { get; set; }
